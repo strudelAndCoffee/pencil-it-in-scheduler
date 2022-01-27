0644d83d7ed4162689b5d3e32330c69df6f4721f
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-01-27 00:33:20 -0600

    started function to add future class to any time block with id later than current hour

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var displayEl = document.querySelector(".container");
+var timeBlocksEl = document.getElementsByClassName("row");
+var timeBlocksArr = [];
+
 var today = moment().format("dddd, MMMM Do");
 $("#currentDay").text(today);
+var rightNow = moment().format("H");
 
 var createTimeBlock = function() {
     for (var i = 9; i <= 17; i++) {
@@ -13,9 +17,8 @@ var createTimeBlock = function() {
         timeBlock.appendChild(hourEl);
 
         var inputEl = document.createElement("textarea");
-        var inputId = "input-" + i;
-        inputEl.setAttribute("id", inputId);
-        inputEl.className = "col-9 row";
+        inputEl.setAttribute("id", i);
+        inputEl.className = "col-9 row description";
         timeBlock.appendChild(inputEl);
 
         var saveEl = document.createElement("div");
@@ -24,8 +27,23 @@ var createTimeBlock = function() {
         saveEl.setAttribute("id", saveId);
         timeBlock.appendChild(saveEl);
 
+        timeBlocksArr.push(timeBlock);
         displayEl.appendChild(timeBlock);
     }
 };
 
+var timeBlockStatus = function(hour) {
+
+    for (var i = 0; i < timeBlocksArr.length; i++) {
+        var thisBlock = timeBlocksArr[i].querySelector(".row");
+        var thisBlockId = thisBlock.getAttribute("id");
+        var thisClass = thisBlock.getAttribute("class");
+
+        if (thisBlockId > hour) {
+            thisBlock.className = thisClass + " future";
+        }
+    }
+}
+
 createTimeBlock();
+timeBlockStatus(rightNow);
