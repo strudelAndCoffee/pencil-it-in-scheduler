646ab2bb131def65786ee957813ad28537f8b14a
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-01-26 23:54:17 -0600

    time block sizing and position uses bootstrap, changed style to built-in classes

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
+var displayEl = document.querySelector(".container");
 var today = moment().format("dddd, MMMM Do");
 $("#currentDay").text(today);
-
-var displayEl = document.querySelector(".container");
 
 var createTimeBlock = function() {
     for (var i = 9; i <= 17; i++) {
         var timeBlock = document.createElement("div");
-        timeBlock.className = "time-block";
+        timeBlock.className = "col-12 d-flex justify-content-end align-items-stretch";
 
         var hourEl = document.createElement("h3");
-        hourEl.className = "time-block-hour";
+        hourEl.className = "col-2 hour";
         hourEl.textContent = moment().hour(i).format("hA");
         timeBlock.appendChild(hourEl);
 
         var inputEl = document.createElement("textarea");
         var inputId = "input-" + i;
         inputEl.setAttribute("id", inputId);
-        inputEl.className = "time-block-input";
+        inputEl.className = "col-9 row";
         timeBlock.appendChild(inputEl);
 
-        var saveEl = document.createElement("p");
+        var saveEl = document.createElement("div");
         var saveId = "save-" + i;
-        saveEl.className = "time-block-save";
+        saveEl.className = "col-1 saveBtn";
         saveEl.setAttribute("id", saveId);
         timeBlock.appendChild(saveEl);
 
