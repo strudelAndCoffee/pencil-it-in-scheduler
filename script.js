44ba2eac5d5814807cf050eb6ef0eaccc07757d0
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-01-28 01:12:19 -0600

    fixed save function, every save pushes current text of all time blocks to time block array, array is saved to local storage and reset after every save

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 var displayEl = document.querySelector(".container");
 var timeBlockEl = document.getElementsByClassName("time-block");
-
-var newTimeBlocks = [];
-// var savedTimeBlocks = ["", "", "", "", "", "", "", "", ""];
-var currentTimeBlocks = [];
-var savedTimeBlocks = JSON.parse(localStorage.getItem("time-blocks"));
-
 var today = moment().format("dddd, MMMM Do");
 $("#currentDay").text(today);
 var rightNow = moment().format("H");
 
+var timeBlocks = [];
+var savedTimeBlocks = JSON.parse(localStorage.getItem("time-blocks"));
+var newTimeBlocks = [];
+
 var createTimeBlock = function() {
 
     for (var i = 0; i < 9; i++) {
-        var timeBlock = document.createElement("div");
-        timeBlock.setAttribute("id", i);
-        timeBlock.className = "col-12 d-flex justify-content-end align-items-stretch row";
+        var timeBlockEl = document.createElement("div");
+        timeBlockEl.setAttribute("id", i);
+        timeBlockEl.className = "col-12 d-flex justify-content-end align-items-stretch row";
 
         var hourEl = document.createElement("h3");
         hourEl.className = "col-2 time-block-hour";
         hourEl.textContent = moment().hour(9 + i).format("hA");
-        timeBlock.appendChild(hourEl);
+        timeBlockEl.appendChild(hourEl);
 
         var inputEl = document.createElement("textarea");
         inputEl.setAttribute("id", "input-" + (9 + i));
         inputEl.className = "col-9 description";
-        timeBlock.appendChild(inputEl);
+        timeBlockEl.appendChild(inputEl);
 
         var saveEl = document.createElement("div");
         saveEl.className = "col-1 d-flex justify-content-center align-items-center saveBtn";
@@ -34,9 +32,9 @@ var createTimeBlock = function() {
         saveBtn.setAttribute("id", (9 + i));
         saveBtn.className = "oi oi-folder save-click";
         saveEl.appendChild(saveBtn);
-        timeBlock.appendChild(saveEl);
+        timeBlockEl.appendChild(saveEl);
 
-        newTimeBlocks.push(timeBlock);
+        newTimeBlocks.push(timeBlockEl);
     }
 };
 
@@ -62,68 +60,90 @@ var timeBlockStatus = function(rightNow) {
     }
 };
 
-var saveTimeBlock = function(event) {
+var loadTimeBlocks = function() {
+
+    if (!timeBlocks) {
+        console.log("nothing saved in local storage");
+        return;
+    }
+    else {
+        for (var i = 0; i < timeBlocks.length; i++) {
+            var thisBlock = document.getElementById(i);
+            var thisText = thisBlock.querySelector("textarea");
+
+            if (!timeBlocks[i].text) {
+                console.log("no text here");
+                thisText.value = " ";
+            } else {
+                thisText.value = timeBlocks[i].text;
+            }
+        }
+    }
+};
+
+var saveBtnHandler = function(event) {
     var target = event.target;
 
     if (target.matches(".save-click")) {
 
         var saveId = target.getAttribute("id");
+        var textValue = displayEl.querySelector("#input-" + saveId).value;
         var i = target.getAttribute("data-div-id");
         parseInt(i);
-        var textValue = displayEl.querySelector("#input-" + saveId).value;
+
         var timeBlockObj = {
             id: i,
             text: textValue
         };
-        currentTimeBlocks[i] = timeBlockObj;
-        localStorage.setItem("time-blocks", JSON.stringify(currentTimeBlocks));
 
-        // if (!currentTimeBlocks) {
-        //     console.log("nothing saved in local storage");
-        //     return;
-        // } else {
-        //     currentTimeBlocks[i] = textValue;
-        // };
-
-        
-
-        // for (var i = 0; i < newTimeBlocks.length; i++) {
-        //     var timeBlockEl = document.getElementById(i);
-        //     var thisSaveId = timeBlockEl.querySelector(".save-click").getAttribute("id");
-        //     var thisText = timeBlockEl.querySelector("textarea").value;
-        //     if (thisSaveId === saveId) {
-        //         savedTimeBlocks[i] = timeBlockValue;
-        //     } else {
-        //         savedTimeBlocks[i] = thisText;
-        //     }      
-        // } 
+        saveTimeBlock(timeBlockObj);        
     }
 };
 
-var loadTimeBlocks = function() {
-    
-    if (!savedTimeBlocks) {
-        console.log("nothing saved in local storage");
-        for (var i = 0; i < newTimeBlocks.length; i++) {
-            var tempObj = {
-                id: i,
-                text: ""
+var saveTimeBlock = function(obj) {
+    var thisTimeBlock = {
+        id: obj.id,
+        text: obj.text
+    };
+
+    if (!obj.text) {
+        console.log("no text to save OR deleted text");
+        thisTimeBlock.text = " ";
+    } else {
+        console.log(obj.text);
+    }
+
+    for (var i = 0; i < newTimeBlocks.length; i++) {
+        var blockEl = document.getElementById(i);
+        var currentText = blockEl.querySelector("textarea").value;
+
+        if (i == obj.id) {
+            timeBlocks.push(thisTimeBlock);
+        }
+        else if (i != obj.id && !currentText) {
+            var tempBlock = {
+                id: i.toString(),
+                text: " "
             };
-            currentTimeBlocks.push(tempObj);
+            timeBlocks.push(tempBlock);
         }
-        return;
-    }
-    else {
-        for (var i = 0; i < savedTimeBlocks.length; i++) {
-            var thisTimeBlock = document.getElementById(i);
-            var thisText = thisTimeBlock.querySelector("textarea");
-            thisText.value = savedTimeBlocks[i].text;
+        else if (i != obj.id && currentText) {
+            var currentBlock = {
+                id: i.toString(),
+                text: currentText
+            };
+            timeBlocks.push(currentBlock);
+        }
+        else {
+            console.log("something went wrong. current text: " + currentText);
         }
     }
-};
+
+    localStorage.setItem("time-blocks", JSON.stringify(timeBlocks));
+    timeBlocks = [];
+}
 
 createTimeBlock();
 timeBlockStatus(rightNow);
 loadTimeBlocks();
-
-displayEl.addEventListener("click", saveTimeBlock);
+displayEl.addEventListener("click", saveBtnHandler);
